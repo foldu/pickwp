9504fd316a6b56a68e7b4ced50b226b28ce34ad5
foldu <foldu@protonmail.com>
2019-09-19 14:34:32 +0200

    Add FIXME

--- src/ipc.rs
+++ src/ipc.rs
@@ -4,6 +4,8 @@ use oneshot_reqrep::Req;
 use serde::{Deserialize, Serialize};
 use structopt::StructOpt;
 
+// FIXME: Use a proper ipc mechanism that hopefully works with std::future
+
 pub const SOCK_PATH: &str = "\0pickwp";
 
 #[derive(StructOpt, Debug, Serialize, Deserialize, Copy, Clone)]
